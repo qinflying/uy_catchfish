7d1225b1a869751f3e7cee5badd342106c0a1585
qinflying <qinflying@outlook.com>
2018-08-13 22:59:46 +0800

    鱼群生成点设置
    Signed-off-by: qinflying <qinflying@outlook.com>

---
+++ Assets/Scripts/FishGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishGenerator : MonoBehaviour {
+    //鱼的挂靠点
+    public Transform m_FishPanel;
+    //地点列表
+    public RectTransform[] m_GeneratePositions;
+    //鱼的预制体
+    public GameObject[] m_FishPrefabs;
+
+	void Start () {
+        InvokeRepeating("ToGenerate", 0, 1);
+	}
+
+    void ToGenerate() { 
+    
+    }
+}
