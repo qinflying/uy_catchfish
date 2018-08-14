b4ecbee85d3cb0159698596fea3e656771dad0c9
qinflying <qinflying@outlook.com>
2018-08-15 00:31:57 +0800

    实现切换武器功能，降级武器出现一次切换bug
    Signed-off-by: qinflying <qinflying@outlook.com>

---
+++ Assets/Scripts/GameContoller.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameContoller : MonoBehaviour
+{
+    //武器数组，威力从小到大
+    public GameObject[] m_GunArrays;
+    //当前威力索引
+    private int _currentPowerIndex = 0;
+    //最大威力索引
+    private int _currentMaxPowerIndex = -1;//-1为未设置
+    //当前武器索引
+    private int _gunIndex = 0;
+
+    public int currentMaxPowerIndex
+    {
+        get
+        {
+            if (_currentMaxPowerIndex == -1)
+            {
+                int level = 0;
+                foreach (GameObject gun in m_GunArrays)
+                {
+                    level += gun.GetComponent<GunAttr>().powerLevel;
+                }
+                _currentMaxPowerIndex = level;
+            }
+            return _currentMaxPowerIndex;
+        }
+    }
+    //当前威力等级
+    public int currentPowerLv
+    {
+        get
+        {
+            return _currentPowerIndex + 1;
+        }
+    }
+    //当前武器等级
+    public int currentGunLv
+    {
+        get
+        {
+            return _gunIndex + 1;
+        }
+    }
+
+    //当前消耗
+    public int currentCost
+    {
+        get
+        {
+            if (currentGun)
+            {
+                return currentGun.GetComponent<GunAttr>().GetCurrentCost();
+            }
+            return 0;
+        }
+    }
+
+    //当前枪
+    public GameObject currentGun
+    {
+        get
+        {
+            if (_gunIndex >= 0 && _gunIndex < m_GunArrays.Length)
+            {
+                return m_GunArrays[_gunIndex];
+            }
+            return null;
+        }
+    }
+
+    //武器提升
+    public void OnUpgradeGun()
+    {
+        if (currentGun)
+        {
+            GunAttr oGunAttr = currentGun.GetComponent<GunAttr>();
+            bool bUpgrade = oGunAttr.AddPower();
+            //当前枪切升级威力成功
+            if (bUpgrade)
+            {
+                _currentPowerIndex++;
+            }
+            else
+            {
+                //当前枪已经是最大威力，换枪
+                _gunIndex++;
+                if (_gunIndex >= m_GunArrays.Length)
+                {
+                    _gunIndex = 0;
+                    _currentPowerIndex = 0;
+                }
+                else
+                {
+                    _currentPowerIndex++;
+                }
+                currentGun.GetComponent<GunAttr>().initPower();
+                switchGun();
+            }
+        }
+    }
+
+    //武器降级
+    public void OnMinusGun()
+    {
+        if (currentGun)
+        {
+            GunAttr oGunAttr = currentGun.GetComponent<GunAttr>();
+            bool bMinus = oGunAttr.MinusPower();
+            if (bMinus)
+            {
+                _currentPowerIndex--;
+            }
+            else
+            {
+                _gunIndex--;
+                if (_gunIndex < 0)
+                {
+                    _gunIndex = m_GunArrays.Length - 1;
+                    if (currentMaxPowerIndex > 0)
+                    {
+                        _currentPowerIndex = currentMaxPowerIndex - 1;
+                    }
+                    else
+                    {
+                        _currentPowerIndex = 0;
+                    }
+
+                }
+                else
+                {
+                    _currentPowerIndex--;
+                }
+                currentGun.GetComponent<GunAttr>().initPower();
+                switchGun();
+            }
+        }
+    }
+
+    private void switchGun() {
+        foreach (GameObject gun in m_GunArrays) {
+            gun.SetActive(gun == currentGun);
+        }
+    }
+}
---
+++ Assets/Scripts/GunAttr.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunAttr : MonoBehaviour
+{
+    //发射消耗数组
+    public int[] m_ShootCosts;
+    private int m_Index = 0;
+
+    //枪处于最大火力
+    public bool isMaxPower()
+    {
+        return m_Index + 1 >= m_ShootCosts.Length;
+    }
+
+    //枪处于最大小力
+    public bool isMinPower()
+    {
+        return m_Index == 0;
+    }
+
+    //初始化枪的威力
+    public void initPower()
+    {
+        m_Index = 0;
+    }
+
+    //增加火力
+    public bool AddPower()
+    {
+        if (m_Index + 1 < m_ShootCosts.Length)
+        {
+            m_Index++;
+            return true;
+        }
+        return false;
+    }
+
+    //减少火力
+    public bool MinusPower()
+    {
+        if (m_Index >= 1)
+        {
+            m_Index--;
+            return true;
+        }
+        return false;
+    }
+
+    //获取当前花费
+    public int GetCurrentCost()
+    {
+        if (m_Index >= 0 && m_Index < m_ShootCosts.Length)
+        {
+            return m_ShootCosts[m_Index];
+        }
+        return 0;
+    }
+
+    public int powerLevel {
+        get {
+            return m_ShootCosts.Length;
+        }
+    }
+}
